b24606d54ff1a5c12e7d3e0a0a476619c3f098e0
Aditya Bhagwat <aditya.bhagwat@mpi-bn.mpg.de>
2019-07-30 11:23:56 +0200

    Fix zzz.R

--- R/04_score_cas9s.R
+++ R/04_score_cas9s.R
@@ -1,13 +1,29 @@
+#' Calculate azimuth ontargetscores
+#' @param  contextseqs character vector
+#' @return numeric vector
+#' @export
+#' @examples
+#' if (reticulate::py_module_available('azimuth')){
+#'     contextseqs <- c('TGCCCTTATATTGTCTCCAGCAGAAGGTGT', 
+#'                     'CCAAATATTGTCAAGTTGACAACCAGGAAT')
+#'     calc_ontargetscore(contextseqs)
+#' }
+calc_ontargetscore <- function(contextseqs){
+    azimuth$model_comparison$predict(numpy$array(contextseqs))
+}
+
 #' Add cas9 ontarget scores
 #' @param cas9dt data.table(chr, cas9start, cas9end, strand)
 #' @param bsgenome BSGenome object, e.g. BSgenome.Mmusculus.UCSC.mm10::Mmusculus
 #' @return data.table(chr, cas9start, cas9end, strand)
 #' @examples
-#' require(magrittr)
-#' bsgenome <- BSgenome.Mmusculus.UCSC.mm10::Mmusculus
-#' bedfile <- system.file('extdata/SRF_sites.bed', package = 'crisprapex')
-#' cas9dt <- read_bed(bedfile) %>% slop_fourways() %>% find_cas9s(bsgenome)
-#' cas9dt %>% add_contextseq()
+#' if (reticulate::py_module_available('azimuth')){
+#'     require(magrittr)
+#'     bsgenome <- BSgenome.Mmusculus.UCSC.mm10::Mmusculus
+#'     bedfile <- system.file('extdata/SRF_sites.bed', package = 'crisprapex')
+#'     cas9dt <- read_bed(bedfile) %>% slop_fourways() %>% find_cas9s(bsgenome)
+#'     cas9dt %>% add_ontargetscores()
+#' }
 #' @export
 add_ontargetscores <- function(cas9dt, bsgenome){
     message('\tScore cas9 sites (https://github.com/MicrosoftResearch/Azimuth)')
@@ -17,9 +33,9 @@ add_ontargetscores <- function(cas9dt, bsgenome){
     cas9dt [ !is.na(cas9start) & strand=='-', contextend   := cas9end  +4,                                               ]
     cas9dt [ !is.na(cas9start)              , contextseq   := range2seq(chr, contextstart, contextend, strand, bsgenome) ]
     cas9dt [                                , c('contextstart', 'contextend') := NULL                                    ]
-    cas9dt[]
     scoredt <- cas9dt [!is.na(cas9start) , .(contextseq = unique(contextseq))  ]
-    scoredt [, ontargetscore := azimuth$model_comparison$predict(numpy$array(scoredt$contextseq)) ]
+    #scoredt [, ontargetscore := azimuth$model_comparison$predict(numpy$array(scoredt$contextseq)) ]
+    scoredt [, ontargetscore := calc_ontargetscore(contextseq) ]
     cas9dt %<>% merge(scoredt, by = 'contextseq', all.x = TRUE)
     cas9dt [ , contextseq := NULL ]
     cas9dt[]
--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,6 @@
+azimuth <- NULL
+numpy   <- NULL
+
 .onload <- function(libname, pkgname){
     azimuth <<- reticulate::import("azimuth", delay_load = TRUE)
     numpy   <<- reticulate::import("numpy",   delay_load = TRUE)
