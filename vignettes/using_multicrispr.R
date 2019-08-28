a4c4b04cbd3aec3432c35634701253b81d2083cc
Aditya Bhagwat <aditya.bhagwat@mpi-bn.mpg.de>
2019-08-28 13:17:47 +0200

    Update vignette

--- vignettes/using_multicrispr.R
+++ vignettes/using_multicrispr.R
@@ -12,23 +12,3 @@ bedfile      <- system.file('extdata/SRF.bed', package = 'multicrispr')
 bsgenome     <- BSgenome.Mmusculus.UCSC.mm10::Mmusculus
 targetranges <- read_bed(bedfile, bsgenome) %>% flank_fourways()
 
-## ----findcas9s-------------------------------------------------------------
-cas9ranges <- find_specific_cas9ranges(targetranges, mismatch = 0)
-
-## ----score_rs1-------------------------------------------------------------
-cas9ranges$score_rs1 <- score_cas9ranges(cas9ranges, ruleset = 1)
-
-## ----score_rs2-------------------------------------------------------------
-python_available <- assertive.reflection::is_unix() & 
-                    reticulate::py_module_available('azimuth')
-if (python_available){
-  cas9ranges$score_rs2 <- score_cas9s(
-                            cas9ranges, ruleset = 2, condaenv = 'default')
-  
-  message('\tFilter for scores > 0.4')
-  cas9ranges %<>% extract(cas9ranges$score > 0.4)
-  cmessage('\t\t%d cas9 seqs across %d ranges', 
-            length(unique(cas9ranges$seqs)), 
-            length(cas9ranges))
-}
-
