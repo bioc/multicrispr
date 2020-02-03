8fcaa71fcccbb1fc00d3207ccea00b06be1a63cd
Aditya <aditya.bhagwat@mpi-bn.mpg.de>
2020-02-03 12:39:33 +0100

    Created gr2dt and dt2gr, which preserve range names

--- R/02_utilities.R
+++ R/02_utilities.R
@@ -41,3 +41,38 @@ name_uniquely <- function(gr, prefix = 'x'){
     gr
 }
 
+#' GRanges <-> data.table
+#' 
+#' @param gr      \code{\link[GenomicRanges]{GRanges-class}}
+#' @param dt      data.table
+#' @param seqinfo \code{\link[GenomeInfoDb]{Seqinfo-class}}
+#' @aliases dt2gr
+#' @examples
+#' bsgenome <- BSgenome.Hsapiens.UCSC.hg38::BSgenome.Hsapiens.UCSC.hg38
+#' gr <- char_to_granges(c(PRNP = 'chr20:4699600:+',             # snp
+#'                             HBB  = 'chr11:5227002:-',             # snp
+#'                             HEXA = 'chr15:72346580-72346583:-',   # del
+#'                             CFTR = 'chr7:117559593-117559595:+'), # ins
+#'                           bsgenome)
+#' (dt <- gr2dt(gr))
+#' (gr <- dt2gr(dt, seqinfo(bsgenome)))
+#' @export
+gr2dt <- function(gr){
+    dt <- as.data.table(gr)
+    if (has_names(gr)){
+        dt[ , names := names(gr)]
+    }
+    dt[]
+}
+
+#' @rdname gr2dt
+#' @export
+dt2gr <- function(dt, seqinfo){
+    gr <- GRanges(dt, seqinfo = seqinfo)
+    if ('names' %in% names(mcols(gr))){
+        names(gr) <- gr$names
+        gr$names <- NULL
+    }
+    gr
+}
+
