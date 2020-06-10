202027d0d11d8ceaaee2cbc964d6f6719b27fab8
Aditya <aditya.bhagwat@mpi-bn.mpg.de>
2020-06-10 16:43:11 +0200

    scripts/02_pe_coords_prnp.R: define EnsDb.Mmusculus() and EnsDb.Hsapiens()

--- inst/scipts/02_pe_coords_prnp.R
+++ inst/scipts/02_pe_coords_prnp.R
@@ -9,10 +9,27 @@
     filter <- ensembldb::filter
     UniprotMappingTypeFilter <- ensembldb::UniprotMappingTypeFilter
     UniprotDbFilter          <- ensembldb::UniprotDbFilter
+    
+    EnsDb.Mmusculus <- function(){
+        hub <- AnnotationHub::AnnotationHub()
+        #hub <- hub[hub$species == 'Mus musculus' & hub$rdataclass == 'EnsDb']
+        #sort(hub$title)
+        #AnnotationHub::query(hub, '98') # 'AH75036'
+        hub[["AH75036"]]
+    }
+    
+    EnsDb.Hsapiens <- function(){
+        hub <- AnnotationHub::AnnotationHub()
+        #hub <- hub[hub$species == 'Homo sapiens' & hub$rdataclass == 'EnsDb']
+        #sort(hub$title)
+        #AnnotationHub::query(hub, '98') # 'AH75011'
+        #AnnotationHub::query(hub, '99') # 'AH78783'
+        hub[["AH78783"]]
+    }
 
     
 # Map identifiers: PRNP -> ENST00000379440
-    ensdb <- multicrispr::EnsDb.Hsapiens.v98()
+    ensdb <- EnsDb.Hsapiens()
     ensdb %<>% filter(UniprotMappingTypeFilter('DIRECT', condition = "=="))
     ensdb %<>% filter(UniprotDbFilter('SWISSPROT', condition = "=="))
     ensembldb::columns(ensdb)
