762ffaa1ee948ef2241e53e91e599591d08241e5
Aditya <aditya.bhagwat@mpi-bn.mpg.de>
2020-07-21 15:39:57 +0200

    read_ranges: add bsgenome argument

--- R/09_write_ranges.R
+++ R/09_write_ranges.R
@@ -1,8 +1,9 @@
 #' Write GRanges to file
-#' @param gr GRanges
-#' @param file 
+#' @param gr \code{\link[GenomicRanges]{GRanges-class}}
+#' @param file file 
 #' @param verbose TRUE (default) or FALSE
-#' @return NULL
+#' @param bsgenome \code{\link[BSgenome]{BSgenome-class}}
+#' @return \code{\link[GenomicRanges]GRanges-class} for read_ranges
 #' @examples 
 #' # Find PE spacers for 4 clinically relevant loci (Anzalone et al, 2019)
 #'     bsgenome <- BSgenome.Hsapiens.UCSC.hg38::BSgenome.Hsapiens.UCSC.hg38  
@@ -12,12 +13,9 @@
 #'         HEXA = 'chr15:72346580-72346583:-',   # del: tay sachs disease
 #'         CFTR = 'chr7:117559593-117559595:+'), # ins: cystic fibrosis
 #'         bsgenome)
-#'     spacers <- find_pe_spacers(gr, bsgenome)
-#'     targetfile <- file.path(tempdir(), 'targets.txt')
-#'     spacerfile <- file.path(tempdir(), 'spacers.txt')
-#'     write_ranges(gr,      targetfile)
-#'     write_ranges(spacers, spacerfile)
-#'     read_ranges(targetfile)
+#'     file <- file.path(tempdir(), 'gr.txt')
+#'     write_ranges(gr, file)
+#'     read_ranges(file, bsgenome)
 #' @export
 write_ranges <- function(gr, file, verbose=TRUE){
     assert_all_are_dirs(dirname(file))
@@ -29,6 +27,10 @@ write_ranges <- function(gr, file, verbose=TRUE){
 
 #' @rdname write_ranges
 #' @export
-read_ranges <- function(file){
-    data.table::fread(file)
+read_ranges <- function(file, bsgenome){
+    
+    assert_all_are_existing_files(file)
+    assert_is_all_of(bsgenome, 'BSgenome')
+    
+    dt2gr(data.table::fread(file), seqinfo = seqinfo(bsgenome))
 }
