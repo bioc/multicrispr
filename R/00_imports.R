4653dea3e54cd3c17099a756fae82d51be4a8768
Aditya Bhagwat <aditya.bhagwat@uni-marburg.de>
2024-03-27 10:35:20 +0100

    Eliminate assertive.numbers in RELEASE_3_18

--- R/00_imports.R
+++ R/00_imports.R
@@ -1,4 +1,3 @@
-#' @importFrom  assertive.numbers     assert_all_are_less_than
 #' @importFrom  assertive.sets        assert_is_subset
 #' @importFrom  Biostrings            complement  DNAStringSet   
 #' @importFrom  Biostrings            vmatchPattern  vcountPDict
